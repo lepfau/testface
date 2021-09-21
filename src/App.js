8d12dbf7974907a51b0657a45487cb4a941fb429
lepfau <edouard.pfauwadel@gmail.com>
2021-09-21 14:40:04 +0200

    ok deploy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
+import facemodel from "../src/lasttestface.glb"
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 function Model(props) {
-  const { scene } = useGLTF("/finalface.glb");
+  const { scene } = useGLTF(facemodel);
+  
   return <primitive object={scene} />;
 }
 
 export default function App() {
+
+  
   return (
     <HashRouter>
-    <Canvas style={{height:"100vh", backgroundColor:"black"}} pixelRatio={[1, 2]} camera={{ position: [-2, 0, 5], fov: 50 }}>
+    <Canvas style={{height:"100vh", backgroundColor:"black"}}pixelRatio={[1, 2]} camera={{ position: [-2, 0, 5], fov: 50 }}>
       <ambientLight intensity={1} color={"lightyellow"} />
-      {/* <pointLight intensity={0.2} position={[1000, 10000, 10000]} /> */}
+      {/* <spotLight intensity={2} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
+      {/* <pointLight intensity={2} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
       <Suspense fallback={null}>
         <Model />
       </Suspense>

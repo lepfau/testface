4e3e6eca4a828e9957efd0c0abaaa3baef95094e
lepfau <edouard.pfauwadel@gmail.com>
2021-09-21 17:00:33 +0200

    stop day

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
-import facemodel from "../src/lasttestface.glb"
+import facemodel from "../src/stoptoday.glb"
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <HashRouter>
     <Canvas style={{height:"100vh", backgroundColor:"black"}}pixelRatio={[1, 2]} camera={{ position: [-2, 0, 5], fov: 50 }}>
-      <ambientLight intensity={1} color={"lightyellow"} />
+      <ambientLight intensity={1}  />
       {/* <spotLight intensity={2} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
       {/* <pointLight intensity={2} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
       <Suspense fallback={null}>

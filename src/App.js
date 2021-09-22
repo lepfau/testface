1e12aed279fa3292be9199303b288206bf69e336
lepfau <edouard.pfauwadel@gmail.com>
2021-09-22 18:38:36 +0200

    test logos

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ import {
 } from "@react-three/drei";
 import background from "./340452.png";
 import { Html, useProgress } from "@react-three/drei";
-
+import Model from "./Gltfexport";
 import facemodel from "./thisistheone.glb";
+import ReactModel from "./React3dlogo"
+import Jslogo from "./Jslogo"
+import Nodelogo from "./Nodelogo"
 
-function Model(props) {
-  const { scene } = useGLTF(facemodel);
- return(
-    <primitive object={scene} position={[0,0,0]} />
-  )
-}
+// function Model(props) {
+//   const { scene } = useGLTF(facemodel);
+
+//   return(
+
+//     <primitive object={scene} position={[0,0,0]}  />
+//   )
+// }
 
 function Loader() {
   const { progress } = useProgress();
@@ -28,7 +33,7 @@ function Loader() {
 export default function App() {
   return (
     <HashRouter>
-      <Canvas
+      <Canvas powerPreference="high-performance"
         onClick={() => console.log("click")}
         className="canvasstyle"
         style={{
@@ -36,17 +41,23 @@ export default function App() {
           background: `no-repeat center/100% 100% url("https://lh3.googleusercontent.com/proxy/bdGa1NUkXeojFlpl2U0j8dasLIVUUVMCkwBbgRR6q0KrVTTPJsA-26jOceQHMdVRuGwGl2EsLQNYyFepWyfT-EYoF-XIQfqdfaBZ8pJmctkXRhgLl1sMLXt22F9JDlokLhZaSnnGT6bTUSyJ-Bj1kuDvKOvu9tzgXq-CsMM")`,
         }}
         pixelRatio={[1, 2]}
+        anisotropy={8}
         camera={{ position: [0, 0, 10], fov: 30}}
       >
         <ambientLight intensity={0.3} />
         {/* <spotLight intensity={1} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
         {/* <pointLight intensity={1} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
         <Suspense fallback={<Loader />}>
+   <group>
           <Model/>
+   <ReactModel scale={2} position={[-1, 0, 0]}/>
+   <Jslogo />
+   <Nodelogo/>
+    </group>
           <Environment preset={'sunset'}/>
         </Suspense>
         <OrbitControls autoRotate = {true} autoRotateSpeed = {5} />
-       
+     
       </Canvas>
     </HashRouter>
   );

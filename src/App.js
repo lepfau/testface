7d3830e86996b956e47453b390193b876ef558e9
lepfau <edouard.pfauwadel@gmail.com>
2021-09-22 14:26:50 +0200

    node closest texture

--- src/App.js
+++ src/App.js
@@ -1,72 +1,57 @@
 import React, { Suspense, useRef } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import { Canvas, useFrame } from "@react-three/fiber"
-import { OrbitControls, ContactShadows, Environment, useGLTF } from "@react-three/drei";
-import background from "./340452.png"
-import { Html, useProgress } from '@react-three/drei'
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  ContactShadows,
+  Environment,
+  useGLTF,
+} from "@react-three/drei";
+import background from "./340452.png";
+import { Html, useProgress } from "@react-three/drei";
 
-import facemodel from "./finaldefinitive.glb"
-
+import facemodel from "./thisistheone.glb";
 
 function Model(props) {
   const { scene } = useGLTF(facemodel);
-  return <primitive object={scene}/>;
+  return <primitive object={scene} position={[0, 0, 0]} />;
+}
+
+function Model2(props) {
+  const { scene2 } = useGLTF(facemodel);
+  return <primitive object={scene2} position={[2, 0, 0]} />;
 }
 
 function Loader() {
-  const { progress } = useProgress()
-  return <Html center>{progress} % loaded</Html>
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
 }
 
 export default function App() {
-
   return (
     <HashRouter>
-    <Canvas  onClick={() => console.log("click")}  className="canvasstyle" style={{height:"100vh", background: `no-repeat center/100% 100% url("https://lh3.googleusercontent.com/proxy/bdGa1NUkXeojFlpl2U0j8dasLIVUUVMCkwBbgRR6q0KrVTTPJsA-26jOceQHMdVRuGwGl2EsLQNYyFepWyfT-EYoF-XIQfqdfaBZ8pJmctkXRhgLl1sMLXt22F9JDlokLhZaSnnGT6bTUSyJ-Bj1kuDvKOvu9tzgXq-CsMM")`}}pixelRatio={[1, 2]} camera={{ position: [0, 0, 10], fov: 30 }}>
-      <ambientLight intensity={0.8}  />
-      {/* <spotLight intensity={2} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
-      {/* <pointLight intensity={2} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
-      <Suspense fallback={<Loader/>} >
-        <Model position={[10, 100, 10]} />
-   
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+      <Canvas
+        onClick={() => console.log("click")}
+        className="canvasstyle"
+        style={{
+          height: "100vh",
+          background: `no-repeat center/100% 100% url("https://lh3.googleusercontent.com/proxy/bdGa1NUkXeojFlpl2U0j8dasLIVUUVMCkwBbgRR6q0KrVTTPJsA-26jOceQHMdVRuGwGl2EsLQNYyFepWyfT-EYoF-XIQfqdfaBZ8pJmctkXRhgLl1sMLXt22F9JDlokLhZaSnnGT6bTUSyJ-Bj1kuDvKOvu9tzgXq-CsMM")`,
+        }}
+        pixelRatio={[1, 2]}
+        camera={{ position: [0, 0, 10], fov: 30 }}
+      >
+        <ambientLight intensity={0.7} />
+        {/* <spotLight intensity={2} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
+        {/* <pointLight intensity={1} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
+        <Suspense fallback={<Loader />}>
+          <Model />
+         
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
     </HashRouter>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import React, { Suspense } from "react";
 // import { Canvas } from "react-three-fiber";
@@ -76,17 +61,13 @@ export default function App() {
 // import { HashRouter, Route, Switch } from "react-router-dom";
 // import background from "./340452.png"
 
-
 // function Model(props) {
 //   const { scene } = useGLTF(facemodel);
 //   return <primitive object={scene}/>;
 // }
 
-
-
 // export default function App() {
 
-  
 //   return (
 //     <HashRouter>
 //     <Canvas  onClick={() => console.log("click")}  className="canvasstyle" style={{height:"100vh", background: `no-repeat center/100% 100% url(${background})`}}pixelRatio={[1, 2]} camera={{ position: [-2, 0, 5], fov: 35 }}>
@@ -95,7 +76,7 @@ export default function App() {
 //       {/* <pointLight intensity={2} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
 //       <Suspense fallback={null} >
 //         <Model />
-   
+
 //       </Suspense>
 //       <OrbitControls />
 //     </Canvas>

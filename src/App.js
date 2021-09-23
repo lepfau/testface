ebe2d10549113d249b3e2316f53642349bd83c46
lepfau <edouard.pfauwadel@gmail.com>
2021-09-23 11:37:23 +0200

    animations ok a mettre au propre

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
@@ -10,20 +10,14 @@ import {
 } from "@react-three/drei";
 import background from "./340452.png";
 import { Html, useProgress } from "@react-three/drei";
-import Model from "./Gltfexport";
-import facemodel from "./thisistheone.glb";
+import Model from "./Animationdeff";
+
 import ReactModel from "./React3dlogo"
 import Jslogo from "./Jslogo"
 import Nodelogo from "./Nodelogo"
+import "./App.css";
 
-// function Model(props) {
-//   const { scene } = useGLTF(facemodel);
 
-//   return(
-
-//     <primitive object={scene} position={[0,0,0]}  />
-//   )
-// }
 
 function Loader() {
   const { progress } = useProgress();
@@ -31,65 +25,52 @@ function Loader() {
 }
 
 export default function App() {
+
+const [smile, setSmile] = useState(false);
+const [mouth, setMouth] = useState(false);
+const [rock, setRock] = useState(false);
+const [sad, setSad] = useState(false);
+const [doubt, setDoubt] = useState(false)
+
   return (
     <HashRouter>
+
+<div className="btn_container">
+       <button onClick={() => {setSmile(!smile); setMouth(false); setRock(false); setSad(false); setDoubt(false)}}>smile</button>
+       <button onClick={() => {setMouth(!mouth); setSmile(false); setRock(false); setSad(false); setDoubt(false)}}>open mouth</button>
+       <button onClick={() => {setRock(!rock); setSmile(false); setMouth(false); setSad(false); setDoubt(false)}}>the rock</button>
+       <button onClick={() => {setSad(!sad); setSmile(false); setMouth(false); setRock(false); setDoubt(false)}}>sad</button>
+       <button onClick={() => {setDoubt(!doubt); setSmile(false); setMouth(false); setRock(false); setSad(false)}}>doubt</button>
+</div>
+
       <Canvas powerPreference="high-performance"
-        onClick={() => console.log("click")}
+      
         className="canvasstyle"
         style={{
           height: "100vh",
           background: `no-repeat center/100% 100% url("https://lh3.googleusercontent.com/proxy/bdGa1NUkXeojFlpl2U0j8dasLIVUUVMCkwBbgRR6q0KrVTTPJsA-26jOceQHMdVRuGwGl2EsLQNYyFepWyfT-EYoF-XIQfqdfaBZ8pJmctkXRhgLl1sMLXt22F9JDlokLhZaSnnGT6bTUSyJ-Bj1kuDvKOvu9tzgXq-CsMM")`,
         }}
         pixelRatio={[1, 2]}
-        anisotropy={8}
+     
         camera={{ position: [0, 0, 10], fov: 30}}
       >
+       
         <ambientLight intensity={0.3} />
         {/* <spotLight intensity={1} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
         {/* <pointLight intensity={1} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
         <Suspense fallback={<Loader />}>
    <group>
-          <Model/>
+          <Model smile={smile} mouth={mouth} rock={rock} sad={sad} doubt={doubt}/>
    <ReactModel scale={2} position={[-1, 0, 0]}/>
    <Jslogo />
    <Nodelogo/>
     </group>
           <Environment preset={'sunset'}/>
         </Suspense>
-        <OrbitControls autoRotate = {true} autoRotateSpeed = {5} />
+        <OrbitControls autoRotate = {false} autoRotateSpeed = {5} />
      
       </Canvas>
     </HashRouter>
   );
 }
 
-// import React, { Suspense } from "react";
-// import { Canvas } from "react-three-fiber";
-// import facemodel from "../src/fulltestsize.glb"
-// import reactmodel from "../src/react3dd.glb"
-// import { OrbitControls, useGLTF, Plane, useAnimations, } from "@react-three/drei";
-// import { HashRouter, Route, Switch } from "react-router-dom";
-// import background from "./340452.png"
-
-// function Model(props) {
-//   const { scene } = useGLTF(facemodel);
-//   return <primitive object={scene}/>;
-// }
-
-// export default function App() {
-
-//   return (
-//     <HashRouter>
-//     <Canvas  onClick={() => console.log("click")}  className="canvasstyle" style={{height:"100vh", background: `no-repeat center/100% 100% url(${background})`}}pixelRatio={[1, 2]} camera={{ position: [-2, 0, 5], fov: 35 }}>
-//       <ambientLight intensity={0.8}  />
-//       {/* <spotLight intensity={2} position={[20, 50, 100]} color={"lightyellow"}   aoMapIntensity={1} /> */}
-//       {/* <pointLight intensity={2} position={[5, 5, 5]} decay={1} distance={300} shadow={1} color={"lightyellow"} /> */}
-//       <Suspense fallback={null} >
-//         <Model />
-
-//       </Suspense>
-//       <OrbitControls />
-//     </Canvas>
-//     </HashRouter>
-//   );
-// }
